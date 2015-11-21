fa3a0b0e2dd9cf8c77e96863fe0ebee67803ddd0
CodeHunter2006 <CodeHunter2006@gmail.com>
2015-11-21 16:42:04 +0800

    optimize article list

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-5.times do |i|
-	Article.create( source_url: "url ##{i}")
+1.times do |i|
+	#Article.create( source_url: "url ##{i}")
+	Article.create( source_url: "http://mp.weixin.qq.com/s?__biz=MzAwMDE3NjkxMg==&mid=400599477&idx=1&sn=b945b4ca89e7bd1c903b0ad20bfa9a2e&scene=4#wechat_redirect" )
 end
